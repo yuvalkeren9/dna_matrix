23326cae504fd44ecc8a033665a451b419ab7d9d
Yuval Keren <yuvalkeren9@gmail.com>
2025-03-08 19:44:32 +0200

    Adding lookahead_v4
    Signed-off-by: Yuval Keren <yuvalkeren9@gmail.com>

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ def consume_logic_random(dna_list, base):
     return random.choice(filtered_strands)
 
 
+# This version just looks if a selected strand has the next base in its lookahead
 def consume_logic_lookahead_one_v1(dna_list, base):
     assert dna_list
 
@@ -142,12 +143,59 @@ def consume_logic_lookahead_one_v3(dna_list, base):
         print("edge case!")
         return random.choice(filtered_strands)
 
+    # Define a custom order
+    custom_order = shift_string(DNA_BASES, get_next_base(base))
+    # Create a mapping from base to its priority in custom_order
+    order_rank = {base: rank for rank, base in enumerate(custom_order)}
+
     # Pick a strand that will unlock a base that is least common in current synthesizeable (funny word) indices
-    sorted_by_value = sorted(bases_counter_d.items(), key=lambda x: x[1])
+    sorted_by_value = sorted(bases_counter_d.items(), key=lambda x: (x[1], order_rank[x[0]]))
     for curr_base, _ in sorted_by_value:
         for strand in filtered_strands:
             if len(strand) > 1 and strand[1] == curr_base:
                 return strand
+
+    # Did not find any strand with the next base as the next character, returning a random strand
+    return random.choice(filtered_strands)
+
+
+# Can be written with better complexity
+# Same as V2, but instead of picking randomly the next base, pick the longest strand (with the base)
+def consume_logic_lookahead_one_v4(dna_list, base):
+    assert dna_list
+
+    # Filter the strands that begin with the given base
+    filtered_strands = [temp_strand for temp_strand in dna_list if temp_strand.startswith(base)]
+
+    if len(filtered_strands) == 0:
+        #        print("Debug. No base available!") TODO: remove
+        return None
+
+    next_base = get_next_base(base)
+    shifted_dna_bases = shift_string(DNA_BASES, next_base)
+
+    is_base_needed = []
+    for curr_base in shifted_dna_bases[:-1]: # Last char will have special treatment
+        is_base_needed.append(not any(s[0] == curr_base for s in dna_list))
+    temp_counter = 0
+    for strand in dna_list:
+        if strand[0] == shifted_dna_bases[-1]:
+            temp_counter += 1
+            if temp_counter == 2:
+                break
+    is_base_needed.append(temp_counter > 1)
+
+    curr_longest_strand = ""
+    curr_max_len = 0
+    for i, curr_base in enumerate(shifted_dna_bases):
+        for strand in filtered_strands:
+            if len(strand) > 1 and strand[1] == curr_base and (is_base_needed[i] is True):
+                if len(strand) > curr_max_len:
+                    curr_longest_strand = strand
+                    curr_max_len = len(strand)
+
+    if curr_longest_strand != "":
+        return curr_longest_strand
 
     # Did not find any strand with the next base as the next character, returning a random strand
     return random.choice(filtered_strands)
@@ -214,7 +262,7 @@ def synthezise(dna_list, func, should_visualize = False):
 def run_experiment(num_of_strands, len_of_strands, logic_func):
     cycle_results = []
     percent_results = []
-    for _ in range(100):
+    for _ in range(300):
         cycles, percent = run_single_experiment(num_of_strands, len_of_strands, logic_func)
         cycle_results.append(cycles)
         percent_results.append(percent)
@@ -232,29 +280,15 @@ def run_single_experiment(num_of_strands, len_of_strands, logic_func, own_list=N
         dna_list = own_list
 
     dna_list_copy = copy.deepcopy(dna_list)
-    dna_list_copy_2 = copy.deepcopy(dna_list)
 
-    optimal_soultion = get_optimal_row_solution(dna_list_copy_2)
-    print(f"The optimal solution is: {optimal_soultion}")
-
-
-    num_of_cycles = synthezise(dna_list, logic_func)
+    num_of_cycles = synthezise(dna_list, logic_func, should_visualize=False)
     # print(f"The num of cycles for selected logic: {num_of_cycles}")
 
-    num_of_cycles_control_group = synthezise(dna_list_copy, consume_logic_random)
+    num_of_cycles_control_group = synthezise(dna_list_copy, consume_logic_random, should_visualize=False)
     # print(f"The num of cycles for random logic: {num_of_cycles_control_group}")
-
-    # Stats:
-    diff_between_optimal_cycles_tested_logic = num_of_cycles - optimal_soultion
-    diff_between_optimal_cycles_random_logic = num_of_cycles_control_group - optimal_soultion
 
     cycles_saved = num_of_cycles_control_group - num_of_cycles
     percentage_save = num_of_cycles_control_group / num_of_cycles
-
-
-
-    print(f"The amount of cycles saved: {cycles_saved}")
-    print(f"Diff from optimal solution: {diff_between_optimal_cycles_tested_logic}")
 
     return cycles_saved, percentage_save
 
@@ -267,7 +301,7 @@ def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == '__main__':
-    #run_experiment(4, 200, consume_logic_lookahead_one_v3)
+    run_experiment(3, 100, consume_logic_lookahead_one_v1)
 
-    run_single_experiment(5, 5, consume_logic_lookahead_one_v3)
+    #run_single_experiment(5, 20, consume_logic_lookahead_one_v3)
 
